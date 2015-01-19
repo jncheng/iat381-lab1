d6c71a4e3fb06f94f548ebc64baf51b54da1c171
jncheng <jncheng@sfu.ca>
2015-01-19 12:51:56 -0800

    initial commit

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -38,6 +38,10 @@ angular
         templateUrl: 'views/contact.html',
         controller: 'ContactCtrl'
       })
+      .when('/contact', {
+        templateUrl: 'views/contact.html',
+        controller: 'ContactCtrl'
+      })
       .otherwise({
         redirectTo: '/'
       });
